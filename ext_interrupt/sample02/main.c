7f77b9ad4248c812840f16c215b2f68183e66c3d
rikitoro <yoshiaki@sendai-nct.ac.jp>
2017-06-27 13:15:40 +0900

    Create main.c

---
+++ ext_interrupt/sample02/main.c
@@ -0,0 +1,23 @@
+#include <avr/interrupt.h>
+#include "my_interrupt6.h"
+
+#include "sw_intput_interface.h"
+#include "led_output_interface.h"
+#include "delay.h"
+
+int main(void)
+{
+  init_sw();
+  init_led();
+
+  init_int6(); // 外部割り込みINT6の初期化
+  sei();  // 割り込み許可
+
+  while (1) // カウント値を点滅させる
+  {
+    set_led(get_count());
+    delay(600);
+    set_led(0x00);
+    delay(600);
+  }
+}
